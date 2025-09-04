a73ef84d8800c7a969a84210055de78c6d8610c4
UltraSlav <pashaverholantsev@gmail.com>
2025-09-04 19:59:19 +0000

    2123

--- bookmark/src/App.jsx
+++ bookmark/src/App.jsx
@@ -1,34 +1,66 @@
 import { useState } from 'react'
 import './App.css'
+import {nanoid} from 'nanoid'
 
 function App() {
-  const [string, setString] = useState("")
-  const [bookmarks, setBookmarks] = useState([])
+    const [string, setString] = useState("")
+    const [bookmarks, setBookmarks] = useState([])
 
-  const handleChange = (e) => {
-    setString(e.target.value)
-  }
-  const handleAdd = () => {
-    setBookmarks((val) => [string, ...val] ) 
-    console.log(bookmarks);
-  }
+    const categories = {
+        movies: "фильмы",
+        music: "музыка",
+        site: "сайты",
+    }
 
-  const handeleCahnge = (e) => {
-    setString(e.target.value)
-  }
-  return (
-    <div className = "bookmarks">
-      <div className = "input">
-    <input onChange={handeleCahnge} value = {string} type = "text" />
-    <button onClick = {handleAdd} >add bookmarks</button>
-      </div>
-      <div className="bookmark-lisr">
-        {bookmarks.map((el) => (
-          <div className="bookmarks=">{el}</div>
-        ))}
-      </div>
-    </div>
-  )
+    const [category, setCategory] = useState(Object.entries(categories)[0][0])
+
+    const handleChange = (e) => {
+        setString(e.target.value)
+    }
+
+    const handleAdd = () => {
+        const bookmark = {
+            name:string,
+            id:nanoid(),
+            date: new Date(),
+            ready: false,
+            category: category
+            
+        }
+        setBookmarks((val) => [bookmark, ...val])
+        console.log(bookmarks)
+    }
+
+   
+ 
+
+     const handleCatChange = (e) => {
+        setCategory(e.target.value)
+    } 
+    return (
+        <div className="bookmarks">
+            <div className="input">
+                <input onChange = {handleChange} value={string} type="text" />
+                <select onChange = {handleCatChange}  name="" id="">
+                    {Object.entries(categories).map((el) =>(
+                        <option value = {el [0]}>{el[1]}</option>
+                    )
+                    )}
+                </select>
+
+                <button onClick={handleAdd} >click me </button>
+            </div>
+            <div className="bookmark-list">
+                {bookmarks.map((el) => (
+                    <div className='rod'>
+                        <input className='bookmark_ready' type="checkbox" checked={el.ready} />
+                        <span className="span-name">{el.name}</span>
+                        <span className="span-cat">{categories[el.category]}</span>
+                        <span className="span-date">{el.date.toISOString()}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
-
 export default App
